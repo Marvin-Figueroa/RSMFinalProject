ab64634dec71037377e458ea71c0daeb76f417f1
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-29 22:45:55 -0600

    Scaffold models from database using Database First approach

---
+++ RSMFinalProject.Server/Models/BusinessEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace RSMFinalProject.Server.Models;
+
+public partial class BusinessEntity
+{
+    /// <summary>
+    /// Primary key for all customers, vendors, and employees.
+    /// </summary>
+    public int BusinessEntityId { get; set; }
+
+    /// <summary>
+    /// ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.
+    /// </summary>
+    public Guid Rowguid { get; set; }
+
+    /// <summary>
+    /// Date and time the record was last updated.
+    /// </summary>
+    public DateTime ModifiedDate { get; set; }
+
+    public virtual Person? Person { get; set; }
+
+    public virtual Store? Store { get; set; }
+
+}
+
