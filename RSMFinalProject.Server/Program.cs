8e93320e5db6d2e2e31143ae3d1ce50d64616601
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-04-30 23:42:07 -0600

    Build Sales API endpoint with query param filters

--- RSMFinalProject.Server/Program.cs
+++ RSMFinalProject.Server/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RSMFinalProject.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AdventureWorks2022Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("localServer"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
