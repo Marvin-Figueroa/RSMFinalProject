ef3a72981075353faa4def7f2c6201dcbfefae7e
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-05-01 21:23:13 -0600

    Build navbar

--- rsmfinalproject.client/src/App.tsx
+++ rsmfinalproject.client/src/App.tsx
@@ -1,10 +1,11 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
 
     return (
         <Grid templateAreas={`"nav" "main"`}>
-            <GridItem area='nav' bg='gold'>Nav</GridItem>
+            <GridItem area='nav'><NavBar/></GridItem>
             <GridItem area='main' bg='tomato'>Main</GridItem>
         </Grid>
     );
